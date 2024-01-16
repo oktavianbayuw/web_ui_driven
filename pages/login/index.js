91d0a2c979c954ed96697697a13dbea54d56ed54
aravikapra <aravikaprakusiya@gmail.com>
2024-01-16 08:04:36 +0700

    upload UI

---
+++ pages/login/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../../components/navbar'
+import Head from "next/head";
+import Login from '../../components/login';
+
+export default function index() {
+  return (
+    <><Head>
+          <title>Nextly - Free Nextjs & TailwindCSS Landing Page Template</title>
+          <meta
+              name="description"
+              content="Nextly is a free landing page template built with next.js & Tailwind CSS" />
+          <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar></Navbar>
+      <Login></Login></>
+  )
+}
