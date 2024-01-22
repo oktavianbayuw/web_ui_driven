40c0b6df107258d64aeb128d45cf22c2c4723cf3
Oktavian Bayu Widodo <oktavianbayuwidodo7@gmail.com>
2024-01-22 15:28:25 +0700

    refactor sidebar
    remove link to "penelitian"

--- components/Layouts/admin_sidebar.js
+++ components/Layouts/admin_sidebar.js
@@ -2,10 +2,10 @@ import Sidebar from "../Fragments/sidebar";
 
 const AdminSidebar = () => {
   const navigation = [
-    ['Dashboard', '/admin/dashboard'],
-    ['Profile', '/admin/profile'],
-    ['Kegiatan Kampus', '/admin/kegiatan/penelitian'],
-    ['Info Kampus', '/admin/info-kampus'],
+    ["Dashboard", "/admin/dashboard"],
+    ["Profile", "/admin/profile"],
+    ["Kegiatan Kampus", "/admin/kegiatan"],
+    ["Info Kampus", "/admin/info-kampus"],
   ];
   return (
     <>
