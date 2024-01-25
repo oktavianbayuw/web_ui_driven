e203d696983086f849d6752bd9b05055456b4ffe
aravikapra <aravikaprakusiya@gmail.com>
2024-01-25 10:01:11 +0700

    update Slider

---
+++ components/Fragments/sliderImage.js
@@ -0,0 +1,27 @@
+// ImageSlider.js
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const ImageSlider = ({ images }) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <Slider {...settings}>
+      {images.map((image, index) => (
+        <div key={index} className="w-full h-64">
+          <img src={image} alt={`Slide ${index + 1}`} className="w-full h-full object-cover" />
+        </div>
+      ))}
+    </Slider>
+  );
+};
+
+export default ImageSlider;
