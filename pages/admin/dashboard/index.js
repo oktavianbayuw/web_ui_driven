d915005c0286e17079b687a80291f877161185b9
aravikapra <aravikaprakusiya@gmail.com>
2024-01-25 16:23:45 +0700

    add card static dashboard

--- pages/admin/dashboard/index.js
+++ pages/admin/dashboard/index.js
@@ -76,8 +76,8 @@ export default function Index() {
       <div className="p-4 sm:ml-64">
         {route === "" ? ( // Check if there is no search, render default content
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
-            <div className="p-3 md:col-span-2 lg:col-span-1">
-              <div className="p-4 rounded-lg dark:border-gray-700 mt-14">
+            <div className="md:col-span-2 lg:col-span-1">
+              <div className="rounded-lg dark:border-gray-700 mt-14 mb-14 w-full">
                 <ImageSlider images={images} />
               </div>
               <h2 className="text-lg font-bold mb-2 mt-3">
@@ -100,6 +100,20 @@ export default function Index() {
                 <div className="p-3">
                   <InfoCard schedules={schedules} />
                 </div>
+              </div>
+              <div class="p-3">
+              <div className="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                  <a href="#">
+                      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Visi Misi Universitas Tugu Muda</h5>
+                  </a>
+                  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Kampus kami adalah tempat berkumpulnya orang-orang yang kreatif, dinamis, dan menginspirasi untuk tumbuh bersama.</p>
+                  <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                      Read more
+                      <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                      </svg>
+                  </a>
+              </div>
               </div>
             </div>
           </div>
